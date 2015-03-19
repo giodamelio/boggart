5be5d1954f1e15807ba52c4c17312e6ad2480171
Gio d'Amelio <giodamelio@gmail.com>
2015-03-18 17:45:40 -0700

    Setup node-inspector for debugging

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ var babel = require("gulp-babel");
 var sourcemaps = require("gulp-sourcemaps");
 var insert = require("gulp-insert");
 var nodemon = require("gulp-nodemon");
+var shell = require("gulp-shell");
 
 // Compile our es6 code to es5 with babel
 gulp.task("babel", function() {
     return gulp.src("src/**/*.js")
-        .pipe(insert.prepend("require(\"source-map-support\").install(); require(\"pretty-error\").start();"))
+        .pipe(insert.prepend("require(\"source-map-support\").install();\n\n"))
         .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(sourcemaps.write("."))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("lib"));
 });
 
@@ -25,12 +26,16 @@ gulp.task("server", ["babel"], function() {
     nodemon({
         script: "./lib/main.js",
         watch: "./lib",
-        ext: "js"
+        ext: "js",
+        nodeArgs: "--debug"
     }).on("restart", function() {
         gutil.log("Server restarted");
     });
 });
 
+// Run node inspector
+gulp.task("inspector", shell.task("./node_modules/.bin/node-inspector"));
+
 // Run the dev server
-gulp.task("dev", ["watch", "server"]);
+gulp.task("dev", ["watch", "server", "inspector"]);
 

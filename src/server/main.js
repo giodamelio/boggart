eba60a4e19dbcf4829d0b932b894ffd22752b3d1
Gio d'Amelio <giodamelio@gmail.com>
2015-03-19 16:40:52 -0700

    Serve index on any path

--- src/server/main.js
+++ src/server/main.js
@@ -1,4 +1,6 @@
 var path = require("path");
+var fs = require("fs");
+
 var express = require("express");
 
 var Api = require("./api");
@@ -8,8 +10,15 @@ var server = express();
 // Mount our api
 server.use("/api", Api);
 
-// Send our frontend
-server.use(express.static(path.resolve(__dirname, "../../lib/client/")));
+// Serve our static assets
+var staticPath = path.resolve(__dirname, "../../lib/client/");
+server.use("/static", express.static(staticPath));
+
+// Serve our index on any other path
+var indexPath = path.resolve(__dirname, "../../lib/client/index.html");
+server.get("/**", function(req, res) {
+    res.sendFile(indexPath);
+});
 
 server.listen(3141);
 console.log("Server listening on http://localhost:3141");
